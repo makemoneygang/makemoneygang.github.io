e7e4d39a6b26975f0bc7e67f2e3e43207c8dde0f
KurisuStina <lin123evan@gmail.com>
2022-06-04 22:02:04 -0400

    Working pages
    home, members pages completed.
    mission page not completed.
    about -> contact.

--- app.js
+++ app.js
@@ -1,2 +1,23 @@
 'use strict';
 
+window.onscroll = function(){onScroll()};
+
+function onScroll(){
+    const btns = document.querySelectorAll(".head-btn");
+    if(document.body.scrollTop > 50 || document.documentElement.scrollTop > 50){
+        document.getElementById("mmg-title").style.fontSize = "0px";
+            
+        btns.forEach(btn => {
+            btn.style.height = "50px";
+
+        })
+    }
+    else{
+        document.getElementById("mmg-title").style.fontSize = "30px";
+
+        btns.forEach(btn => {
+            btn.style.height = "100px";
+            
+        })
+    }
+}
